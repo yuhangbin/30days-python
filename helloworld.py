b833d07502f3741a053f9b603bc851766f508021
CharlieYu <yuhangbinccc@gmail.com>
2024-07-17 00:29:08 +0800

    feat: day1

---
+++ helloworld.py
@@ -0,0 +1,25 @@
+# This is a comment
+""" This is multiline comment
+"""
+
+import math
+
+print(2 + 3)             # addition(+)
+print(3 - 1)             # subtraction(-)
+print(2 * 3)             # multiplication(*)
+print(3 / 2)             # division(/)
+print(3 ** 2)            # exponential(**)
+print(3 % 2)             # modulus(%)
+print(3 // 2)            # Floor division operator(//)
+
+print(type(10))          # Int
+print(type(3.14))        # Float
+print(type(1 + 3j))      # Complex number
+print(type('Asabeneh'))  # String
+print(type([1, 2, 3]))   # List
+print(type({'name':'Asabeneh'})) # Dictionary
+print(type({9.8, 3.14, 2.7}))    # Set
+print(type((9.8, 3.14, 2.7)))    # Tuple immutable
+
+
+print("an Euclidian distance between (2, 3) and (10, 8) is: " + str(math.sqrt((2-10)**2 + (3-8)**2)))
