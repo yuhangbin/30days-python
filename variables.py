e7a03a6f119a6a1c964220cefd4be804c1da024c
CharlieYu <yuhangbinccc@gmail.com>
2024-07-18 01:03:29 +0800

    feat: day2

---
+++ variables.py
@@ -0,0 +1,23 @@
+import math
+
+# level 1
+name=input('your name is:')
+music,movie = 'You and I', "your name"
+
+# level 2
+print(type(name)) # str
+print(len(name))
+print(len(name) > 2)
+num_one,num_two = 5,4
+total = num_two + num_one
+diff = num_two - num_one
+division = num_one/num_two
+remiander = num_two % num_one
+exp = num_one * num_two
+floor_division = num_one // num_two # 7//3 = 2
+area_of_cicle = 30**2*math.pi
+circum_of_circle = 2*30*math.pi
+radius = float(input('input radius: '))
+print(radius * 2 * math.pi)
+first_name = input('first name:')
+help('keywords')
