dc5ab2c1c07adc5124e92c9c22144cc8840b7b4e
CharlieYu <yuhangbinccc@gmail.com>
2024-07-19 00:27:45 +0800

    feat: day3

---
+++ operators.py
@@ -0,0 +1,35 @@
+complex = 1+1j
+print(type(complex)) 
+base = int(input('Enter base:'))
+print(type(base))
+height = int(input('Enter height:'))
+print(type(height))
+print('The area of the triangle is :' + str(0.5*base*height))
+
+x = 1
+y = 2*x - 2
+print(y)
+
+slope = (10 - 2)/(6 - 2)
+print('slope:' + str(slope))
+
+x = int(input('x: '))
+y = x**2 + 6*x + 9
+print(y)
+x = -9/x - 6
+print('x: ' + str(x) + ' then 0 = x^2 + 6x + 9')
+
+print(len('python') > len('dragon'))
+
+print(('on' in 'python') and ('on' in 'dragon'))
+
+print(str(float(len('python'))))
+
+print(2%2 == 0)
+print(7//3 == int(2.7))
+
+print("1 1 1 1 1")
+print("2 1 2 4 8")
+print("3 1 3 9 27")
+print("4 1 4 16 64")
+print("5 1 5 25 125")
