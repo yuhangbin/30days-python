7fd5668b6c5db6e596c03018a66ee08e48982148
CharlieYu <yuhangbinccc@gmail.com>
2024-07-30 23:17:08 +0800

    feat: day15

--- type_errors_day15.py
+++ type_errors_day15.py
@@ -1,10 +1,22 @@
-
+# SyntaxError
+print 'hello'
 # NameError
+print(age)
 # IndexError
+numbers = [1]
+print(numbers[2])
 # ModuleNotFoundError
+import maths
 # AttributeError
+maths.pi
 # KeyError
+user = {'name': 'A'}
+user['age']
 # TypeError
+4 + '1'
 # ImportError
+from math import power
 # ValueError
+int('12a')
 # ZeroDivisionError
+1/0
