909608452d8f8ccdd7a31fc0eb4bef9748464d92
CharlieYu <yuhangbinccc@gmail.com>
2024-08-07 23:24:41 +0800

    feat: day23

--- flask_project/main.py
+++ flask_project/main.py
@@ -1,1 +1,13 @@
 print('this is flask project')
+
+# pip install virtualenv
+
+# source env/bin/activate
+
+# pip list
+
+# pip freeze > requirements.txt
+
+# pip install -r requirements.txt
+
+# deactivate
